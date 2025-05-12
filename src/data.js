4f37299aae84a9123c08ff851f327c396e76042c
DiStail88 <lebwebslav@gmail.com>
2025-05-12 17:37:53 +0300

    Сделал hw-2

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const cardList = [
+{
+    id: 1,
+    topic: 'Web Design',
+    title: 'Название задачи',
+    date: '30.10.23',
+    status: 'orange'
+},
+{
+    id: 2,
+    topic: 'Research',
+    title: 'Название задачи',
+    date: '30.10.22',
+    status: 'green'
+},
+{
+    id: 3,
+    topic: 'Copywriting',
+    title: 'Название задачи',
+    date: '30.10.25',
+    status: 'purple'
+}
+]
